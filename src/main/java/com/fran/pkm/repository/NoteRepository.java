e7999f9163876ad22f03e6f7daabfb45d6b3db6c
negi <nezii0331@gmail.com>
2025-10-17 12:57:15 +0800

    feat: add dependancy

--- src/main/java/com/fran/pkm/repository/NoteRepository.java
+++ src/main/java/com/fran/pkm/repository/NoteRepository.java
@@ -22,6 +22,8 @@ public interface NoteRepository{
     boolean delete(String id);
     //use boolean-this way api can respond with 200/404
 
+
+    //while user searching
     List<Note> searchByKeyword(String keyword);
     List<Note> findByStatus(String status);
     List<Note> findByTag(String tag);
